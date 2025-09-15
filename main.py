aa240088f5280eb8cf5394d9a2c972c462f12d4e
awaleed1 <awaleed1@broncos.uncfsu.edu>
2025-09-15 15:30:53 -0400

    Basic Starting Code for Project

---
+++ main.py
@@ -0,0 +1,10 @@
+game_name = "Leviathan Plunge" # TODO, name your game project
+print("Welcome to", game_name + "!") #  TODO, print greetings with game name
+print("============================") #TODO, add or remove equal symbol to align the greetings
+
+# Ask for the character's name
+name = input("Before your quest starts. What is your name?\n")
+
+# Print the name
+print(">" + name)
+print("Wonderful! Welcome", name + ",", "Let us begin the story.")
