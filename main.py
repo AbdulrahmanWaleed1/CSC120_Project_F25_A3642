65fcd9714bf4c708bf87fa091ff653aa52886425
Abdulrahman <awaleed1@broncos.uncfsu.edu>
2025-09-26 10:48:19 -0400

    Slight changes to the print

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-game_name = "Leviathan Plunge" # TODO, name your game project
-print("Welcome to", game_name + "!") #  TODO, print greetings with game name
-print("============================") #TODO, add or remove equal symbol to align the greetings
+game_name = "Leviathan Plunge" # FINISHED, name your game project
+print("Welcome to", game_name + "!") #  FINISHED, print greetings with game name
+print("============================") #FINISHED, add or remove equal symbol to align the greetings
 
 # Ask for the character's name
-name = input("Before your quest starts. What is your name?\n")
+name = input("Before your quest starts. What is your name?\n> ")
 
 # Print the name
-print(">" + name)
 print("Wonderful! Welcome", name + ",", "Let us begin the story.")
